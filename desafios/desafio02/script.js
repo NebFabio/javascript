1ac7c8ae2aa47d3a10fc0903006eaf6ce2e3bd51
unnebell <128543369+NebFabio@users.noreply.github.com>
2023-04-06 15:05:48 -0300

    site verificador de idade pronto e bugs fix

--- desafios/desafio02/script.js
+++ desafios/desafio02/script.js
@@ -1,82 +1,50 @@
-function carregar (){
-    var msg = window.document.getElementById('msg')
-    var img = window.document.getElementById('imagem') 
-    var data = new Date()
-    var hora = data.getHours()
-    msg.innerHTML = `Agora são ${hora} horas.`
-    if (hora >= 0 && hora < 12) {
-        img.src = 'imagens/fotomanha.png'
-        document.body.style.background = '#FDF69E'
-        msg2.innerHTML = `Bom dia!😊`
-    } else if (hora >=12 && hora <= 17) {
-        img.src = 'imagens/fototarde.png'
-        document.body.style.background = '#FDA656'
-        msg2.innerHTML = `Boa tarde!😉`
-    } else {
-        img.src = 'imagens/fotonoite.png'
-        document.body.style.background = '#2E454D'
-        msg2.innerHTML = `Boa noite!😶`
-        var rd = document.getElementById('rd')
-        var link = document.querySelector('a#link')
-        var link2 = document.querySelector('a#link2')
-        rd.style.color = 'white'
-        link.style.color = 'aqua'
-        link2.style.color = 'aqua'
-    }  switch (hora) {
-        case 0:
-            img.src = 'imagens/fotonoite.png'
-            document.body.style.background = '#2E454D'
-            msg2.innerHTML = `<p>Boa madrugada🥱</p>`
-            var rd = document.getElementById('rd')
-            var link = document.querySelector('a#link')
-            var link2 = document.querySelector('a#link2')
-            rd.style.color = 'white'
-            link.style.color = 'aqua'
-            link2.style.color = 'aqua'
-        break
-        case 1:
-            img.src = 'imagens/fotonoite.png'
-            document.body.style.background = '#2E454D'
-            msg2.innerHTML = `<p>Boa madrugada🥱</p>`
-            var rd = document.getElementById('rd')
-            var link = document.querySelector('a#link')
-            var link2 = document.querySelector('a#link2')
-            rd.style.color = 'white'
-            link.style.color = 'aqua'
-            link2.style.color = 'aqua'
-        break
-        case 2:
-            img.src = 'imagens/fotonoite.png'
-            document.body.style.background = '#2E454D'
-            msg2.innerHTML = `<p>Boa madrugada🥱</p>`
-            var rd = document.getElementById('rd')
-            var link = document.querySelector('a#link')
-            var link2 = document.querySelector('a#link2')
-            rd.style.color = 'white'
-            link.style.color = 'aqua'
-            link2.style.color = 'aqua'
-        break 
-        case 3:
-            img.src = 'imagens/fotonoite.png'
-            document.body.style.background = '#2E454D'
-            msg2.innerHTML = `<p>Boa madrugada🥱</p>`
-            var rd = document.getElementById('rd')
-            var link = document.querySelector('a#link')
-            var link2 = document.querySelector('a#link2')
-            rd.style.color = 'white'
-            link.style.color = 'aqua'
-            link2.style.color = 'aqua'
-        break
-        case 4:
-            img.src = 'imagens/fotonoite.png'
-            document.body.style.background = '#2E454D'
-            msg2.innerHTML = `<p>Boa madrugada🥱</p>`
-            var rd = document.getElementById('rd')
-            var link = document.querySelector('a#link')
-            var link2 = document.querySelector('a#link2')
-            rd.style.color = 'white'
-            link.style.color = 'aqua'
-            link2.style.color = 'aqua'
-    }
-    
+function verificar (){
+   var data = new Date()
+   var ano = data.getFullYear() // vai pegar os quatro dígitos do ano
+   var fano = document.getElementById('txtano')
+   var res = document.getElementById('res')
+   if (fano.value.length == 0 || fano.value > ano) {
+        alert('[ERRO] Verifique os dados e tente novamente!')
+   } else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked) {
+            genero = 'Homem'
+            if (idade >=0 && idade < 13) {
+                //criança
+                img.setAttribute('src', 'crian-h.png')
+            } else if (idade >= 12 && idade < 24) {
+                //jovem
+                img.setAttribute('src', 'jovem-h.png')
+            } else if (idade < 50) {
+                //adulto
+                img.setAttribute('src', 'adulto-h.png')
+            } else {
+                //idoso
+                img.setAttribute('src', 'idoso-h.png')
+            }
+        } else if (fsex[1].checked) {
+            genero = 'Mulher'
+            if (idade >=0 && idade < 12) {
+                //criança
+                img.setAttribute('src', 'crian-m.png')
+            } else if (idade < 24) {
+                //jovem
+                img.setAttribute('src', 'jovem-m.png')
+            } else if (idade < 50) {
+                //adulto
+                img.setAttribute('src', 'adulto-m.png')
+            } else {
+                //idoso
+                img.setAttribute('src', 'idoso-m.png')
+            }
+        }
+        //res.style.textAlign = 'center' // centraliza o texto
+        res.innerHTML = `<p>Detectamos ${genero} com ${idade} anos</p>`
+        res.appendChild(img)
+   }   
+
 }
