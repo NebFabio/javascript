540fd1dceb77783e4b8f67ff9fe70cf6cba85b01
unnebell <128543369+NebFabio@users.noreply.github.com>
2023-04-10 15:30:49 -0300

    aula13

---
+++ exercicios/aula13/ambiente.js
@@ -0,0 +1,21 @@
+var c = 1
+do  {
+    console.log('Tudo bem?')
+    c++
+} while (c <= 3)
+
+/* 
+
+var c = 1
+while (c <= 3) {
+    console.log('Tudo bem?')
+    c++
+}
+
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
