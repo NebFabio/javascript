74a462982747c41561bfb6122910477742d724e6
unnebell <128543369+NebFabio@users.noreply.github.com>
2023-04-11 14:15:49 -0300

    desafio 003

--- desafios/desafio03/script.js
+++ desafios/desafio03/script.js
@@ -1,16 +1,32 @@
-function contar (){
-    var inicio = window.document.getElementById('numberini').value;
-    var final = window.document.getElementById('numberfim').value;
-    var passos = window.document.getElementById('numberpass').value;
+function contar() {
+    let inicio = document.getElementById('nini')
+    let final = document.getElementById('nfinal')
+    let passos = document.getElementById('npass')
+    let res = document.getElementById('res')
     
-    while (inicio <= final) {
-        resu.innerHTML = (inicio) + (passos)
+    if (inicio.value.length == 0 || final.value.length == 0 || passos.value.length == 0) {
+        res.innerHTML = 'Impossível contar!'
+        window.alert('[ERRO] Preencha todos os campos!')
+    } else {
+        res.innerHTML = 'Contando: <br>'
+        let i = Number(inicio.value)
+        let f = Number(final.value)
+        let p = Number(passos.value)
+        if (p <= 0) {
+            alert('Passo inválido! Considerando passo 1')
+            p = 1
+        }
+        if (i < f) {
+            //contagem crescente
+            for(let c = i; c <= f; c += p) {
+            res.innerHTML += ` ${c} \u{1F449}`
+        }
+        } else {
+            //contagem regressiva
+            for ( let c = i; c >= f; c-= p) {
+                res.innerHTML += `${c} \u{1f449}`
+            }
+        }
+        res.innerHTML += `\u{1f3c1}`
     }
 }
-
-/* isso vai somar o incio com os passos
-var n1 = Number(inicio) 
-var n2 = Number(passos) 
-var s = n1 + n2
-resu.innerHTML = s
-*/
